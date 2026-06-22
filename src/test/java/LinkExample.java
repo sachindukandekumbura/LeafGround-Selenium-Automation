2af783d71c73624793b45f2897c2382e77c6fdc7
sachindukandekumbura <sachindukandekumbura1@gmail.com>
2026-06-22 10:32:50 -0700

    Added Link Test Case

--- src/test/java/LinkExample.java
+++ src/test/java/LinkExample.java
@@ -1,6 +1,9 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class LinkExample {
       WebDriver driver;
@@ -8,7 +11,17 @@ public class LinkExample {
        public void LinkOpen()
        {
          driver =new ChromeDriver();
+         driver.manage().window().maximize();
          driver.get("https://www.leafground.com/link.xhtml");
+
        }
+       @Test
+    public void Linktest()
+       {
+           WebElement homelink = driver.findElement(By.linkText("Go to Dashboard"));
+           homelink.click();
+           driver.navigate().back();
+       }
+
 
 }
